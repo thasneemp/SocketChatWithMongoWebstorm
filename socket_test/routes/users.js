8c3c9b0c2365f1d9f3bf50f44433ec52124e0741
Muhammed Thasneem <muhammed.thasneem@cabotsolutions.com>
2016-03-26 23:40:54 +0530

    api optimized

--- socket_test/routes/users.js
+++ socket_test/routes/users.js
@@ -7,12 +7,15 @@ var message_exist = "User already exist";
 var message_login_success = "User login Success";
 var message_login_failed = "User login Failed";
 var message_success_insert = "User registration success";
+var message_success_list = "User list";
 var message_failed_insert = "User registration Failed";
 var message_error = "Error occurred";
 
 var message = {
-    message: "",
-    status: false,
+    server_status: {
+        message: "",
+        status: true
+    },
     userDetails: null
 }
 
@@ -25,19 +28,19 @@ router.post('/register', function (req, res, next) {
         name: req.body.name,
         username: req.body.username,
         password: req.body.password,
-        img: req.body.image_url
+        image_url: req.body.image_url == undefined ? "" : req.body.image_url
     }
     console.log(usedt);
     UserD.find({username: usedt.username}, {_id: 1, username: 1}, function (err, userdeatils) {
         if (err) {
-            message.message = message_failed_insert;
-            message.status = false;
+            message.server_status.message = message_failed_insert;
+            message.server_status.status = false;
             res.send(message);
         } else if (userdeatils == undefined || userdeatils == null || userdeatils.length == 0) {
             insertData(usedt, res);
         } else {
-            message.message = message_exist;
-            message.status = false;
+            message.server_status.message = message_exist;
+            message.server_status.status = false;
             message.userDetails = userdeatils;
             res.send(message);
         }
@@ -60,12 +63,12 @@ function insertData(usedt, res) {
 
     userd.save(function (err, userdetails) {
         if (err) {
-            message.message = message_failed_insert;
-            message.status = false;
+            message.server_status.message = message_failed_insert;
+            message.server_status.status = false;
             res.send(message);
         } else {
-            message.message = message_success_insert;
-            message.status = true;
+            message.server_status.message = message_success_insert;
+            message.server_status.status = true;
             message.userDetails = userdetails;
             res.send(message);
         }
@@ -73,12 +76,17 @@ function insertData(usedt, res) {
     });
 }
 
-router.get('/', function (req, res, next) {
-    UserD.find({}, {chat: 0, password: 0}, function (err, details) {
+router.get('/getcontacts', function (req, res, next) {
+
+    var id = req.param("id");
+    UserD.find({_id: {$nin: [id]}}, {chat: 0, password: 0, __v: 0}, function (err, details) {
         if (err) {
             res.send("error");
         } else {
-            res.send(details);
+            message.server_status.message = message_success_list;
+            message.server_status.status = true;
+            message.userDetails = details;
+            res.send(message);
         }
     });
 });
@@ -95,13 +103,13 @@ router.post('/login', function (req, res, next) {
         if (err) {
             res.send(message_error);
         } else if (userdetails == null || userdetails.length == 0) {
-            message.message = message_login_failed;
-            message.status = false;
+            message.server_status.message = message_login_failed;
+            message.server_status.status = false;
             message.userDetails = userdetails;
             res.send(message)
         } else {
-            message.message = message_login_success;
-            message.status = true;
+            message.server_status.message = message_login_success;
+            message.server_status.status = true;
             message.userDetails = userdetails;
             res.send(message)
         }
